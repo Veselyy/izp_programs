414ec82b34f26e29ff6d37a45ccbf12aedb2670c
martinvesely <imthemartinveselyap@gmail.com>
2024-10-13 19:01:47 +0200

    Initial Commit

---
+++ izp_programs/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  izp_programs
+//
+//  Created by Martin Veselý on 13.10.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
